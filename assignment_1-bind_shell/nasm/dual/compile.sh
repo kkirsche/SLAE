2e0ba4689eeb0041c1e11b2eb12a732019e6cfc9
Kevin Kirsche <kev.kirsche@gmail.com>
2018-08-18 10:04:35 -0400

    Add working shellcode

--- assignment_1-bind_shell/nasm/dual/compile.sh
+++ assignment_1-bind_shell/nasm/dual/compile.sh
@@ -6,4 +6,7 @@ nasm -f elf32 -o dual_stack_bind_shell.o dual_stack_bind_shell.nasm
 echo '[+] Linking ...'
 ld -o dual_stack_bind_shell dual_stack_bind_shell.o -fno-stack-protector -shared -z execstack
 
+echo '[+] Dumping shellcode ...'
+objdump -d ./dual_stack_bind_shell|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+
 echo '[+] Done!'
